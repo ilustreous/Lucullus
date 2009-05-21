41be7e5b837604f8a16482b0834af7bc4225cb61
Marcel Hellkamp <marc@gsites.de>
2009-05-21 17:29:13 +0200

    Server start script should not assume you are working with virtualenv

--- start-server.sh
+++ start-server.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 path=`dirname $0`
 path=`cd $path; pwd`
-ppath=`cd $path; cd ..; pwd`
-
-echo "Path is $path. Entering enviroment"
-source $ppath/bin/activate
 
 echo "Shutting down server"
 kill `cat $path/paster.pid`
 
 echo "Starting new server deamon"
 paster setup-app development.ini
-paster serve --pid-file=$path/paster.pid --log-file=$path/paster.log $path/development.ini
+paster serve --reload --pid-file=$path/paster.pid --log-file=$path/paster.log $path/development.ini
 
 echo "Done!"
